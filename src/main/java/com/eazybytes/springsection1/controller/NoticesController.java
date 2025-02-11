ca7153b5156b93f5749bf6809215484b5e4dabcd
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2025-02-11 17:20:04 -0600

    New controllers to show public and private data from the app and user

--- src/main/java/com/eazybytes/springsection1/controller/NoticesController.java
+++ src/main/java/com/eazybytes/springsection1/controller/NoticesController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
-    @GetMapping("/welcome")
-    public String sayWelcome() {
-        return "Welcome to Spring Boot Application with Security";
+public class NoticesController {
+    @GetMapping("/notices")
+    public String getNotices() {
+        return "Here are the notices details from the database";
     }
 }
