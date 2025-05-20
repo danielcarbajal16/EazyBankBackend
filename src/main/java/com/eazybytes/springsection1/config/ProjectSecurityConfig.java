7eaa23a9cb3699c92e48e445efaff88a7c4e48ac
Daniel Carbajal <edudani81@gmail.com>
2025-05-20 00:50:45 -0600

    Unused import and change to not show jpa sql on console

--- src/main/java/com/eazybytes/springsection1/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/springsection1/config/ProjectSecurityConfig.java
@@ -4,7 +4,6 @@ import com.eazybytes.springsection1.exceptionhandling.CustomAccessDeniedHandler;
 import com.eazybytes.springsection1.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
