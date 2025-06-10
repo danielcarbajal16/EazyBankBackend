708eb17e05b6d42f0d1264600f2e8084f495c2c2
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2025-06-10 17:05:15 -0600

    Basic configuration to load customer authorities from database.

--- src/main/java/com/eazybytes/springsection1/model/Customer.java
+++ src/main/java/com/eazybytes/springsection1/model/Customer.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -36,4 +37,8 @@ public class Customer {
     @Column(name = "create_dt")
     @JsonIgnore
     private Date createDt;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Set<Authority> authorities;
 }
