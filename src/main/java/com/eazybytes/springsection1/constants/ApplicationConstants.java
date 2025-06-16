e28496838ed6451f2bf1722a405b2df7156a0c54
Eduardo Daniel Carbajal <eduardo.carbajal@jalisco.gob.mx>
2025-06-16 17:25:43 -0600

    Logic created to generate JWT token

---
+++ src/main/java/com/eazybytes/springsection1/constants/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.eazybytes.springsection1.constants;
+
+public final class ApplicationConstants {
+    public final static String JWT_SECRET_KEY = "JWT_SECRET";
+    public final static String JWT_SECRET_DEFAULT_VALUE = "jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public final static String JWT_HEADER = "Authorization";
+}
